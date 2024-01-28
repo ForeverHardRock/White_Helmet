8aed1f1ba6d6a289140b437f7204f60322599c7f
Andrew <foreverhardrock@yandex.ru>
2024-01-28 23:24:49 +0100

    admin fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,33 @@
+// module.exports = {
+//     entry: '/main/static/main/js/app.js',
+//     mode: "development",
+//     output: {
+//         filename: 'bundle.js',
+//         path: __dirname + '/main/static/dist/',
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.s[ac]ss$/i,
+//                 use: [
+//                     "style-loader",
+//                     "css-loader",
+//                     {
+//                         loader: "sass-loader",
+//                         options: {
+//                             sourceMap: true
+//                         }
+//                     }
+//                 ],
+//             },
+//         ],
+//     },
+// }
+//
+//
+//
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
     entry: '/main/static/main/js/app.js',
     mode: "development",
@@ -10,16 +40,19 @@ module.exports = {
             {
                 test: /\.s[ac]ss$/i,
                 use: [
-                    "style-loader",
-                    "css-loader",
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    }
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
                 ],
             },
         ],
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+    ],
 }
+
+
+
